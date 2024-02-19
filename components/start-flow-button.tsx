8f71ffa53b68f0dfe54b609b10601044407906e2
enzotar <enzotar3000@gmail.com>
2024-02-19 14:19:59 -0800

    update signature request

--- components/start-flow-button.tsx
+++ components/start-flow-button.tsx
@@ -10,6 +10,8 @@ import { convertSocketData } from '@/lib/websocket';
 import { useFlowStore } from '@/stores/loaded-flow';
 import { useFlowRun } from '@/stores/flow-run';
 import { useSocketDataStore } from '@/stores/socket-data';
+import { ed25519 } from '@noble/curves/ed25519';
+import { SignatureRequest } from '@space-operator/client/dist/module/types/ws';
 
 interface StartFlowButtonProps {
   flowId: number;
@@ -52,8 +54,25 @@ export const StartFlowButton: FC<StartFlowButtonProps> = ({
           }
           // handle signature request
           if (ev.event === 'SignatureRequest') {
-            const tx = ev.data.buildTransaction();
-            const pk = new PublicKey(ev.data.pubkey);
+            const req = new SignatureRequest({
+              id: ev.data.id,
+              message: ev.data.message,
+              pubkey: ev.data.pubkey,
+              signatures: ev.data.signatures,
+              flow_run_id: ev.data.flow_run_id,
+              time: '', // not needed
+              timeout: 0, // not needed
+            });
+
+            const pk = new PublicKey(req.pubkey);
+            if (!publicKey.equals(pk)) {
+              throw `different public key:\nrequested: ${
+                req.pubkey
+              }}\nwallet: ${publicKey.toBase58()}`;
+            }
+
+            const tx = req.buildTransaction();
+            console.log('signing', tx);
 
             // sign and check if the wallet has changed the transaction
             const signedTx = await signTransaction(tx);
@@ -62,17 +81,24 @@ export const StartFlowButton: FC<StartFlowButtonProps> = ({
             const signature = signedTx.signatures.find((ele) =>
               ele.publicKey.equals(pk)
             ).signature;
-            
-            const before = tx.serializeMessage();
-            const after = signedTx.serializeMessage();
-            if (!before.equals(after)) {
-              console.log(ev.data.id, tx, signedTx);
-              alert('tx changed');
-              return;
+
+            let newMsg = null;
+            if (
+              !ed25519.verify(
+                signature,
+                Buffer.from(ev.data.message, 'base64'),
+                pk.toBuffer()
+              )
+            ) {
+              console.log('changing transaction');
+              newMsg = signedTx.serializeMessage();
             }
+
             restClient.submitSignature({
               id: ev.data.id,
               signature: base58Encode(signature),
+              new_msg:
+                newMsg != null ? Buffer.from(newMsg).toString('base64') : null,
             });
           }
         },
