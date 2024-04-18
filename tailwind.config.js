1787b7cb00e15651761604fb3f398f2a462f15f0
enzotar <enzotar3000@gmail.com>
2024-04-18 15:36:27 -0700

    move to src

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,6 @@ const { fontFamily } = require('tailwindcss/defaultTheme');
 module.exports = {
   darkMode: ['class'],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
   ],
   prefix: '',
