90f6b01f4e1f7c78f492de8518a2e7a6bddfa352
enzotar <enzotar3000@gmail.com>
2024-04-18 15:22:51 -0700

    update client

--- utils/client.tsx
+++ utils/client.tsx
@@ -2,7 +2,7 @@ import { Client, WsClient } from '@space-operator/client';
 
 export const restClient = new Client({
   // url: 'http://localhost:8080',
-  url: 'https://dev-api.spaceoperator.com',
+  host: 'https://dev-api.spaceoperator.com',
 });
 
 export const wsClient = new WsClient({
